13f75d33d7826b13da4348b59fde3e9026b56d64
Andreas Lambropoulos <andlamb2002@gmail.com>
2024-05-06 13:17:17 -0400

    Improved expense table formatting

--- client/src/Components/ExpenseTable.jsx
+++ client/src/Components/ExpenseTable.jsx
@@ -1,80 +1,175 @@
 import React, { useState } from 'react';
 import { useAuth } from '../Contexts/authContext';
 import axios from 'axios';
-import API_URL from '../config'; 
+import API_URL from '../config';
 
 function ExpenseTable({ expenses, setExpenses, budgets, fetchBudgetsAndExpenses, setAlertMessage }) {
     const { user, refreshSession } = useAuth();
-    const [newExpense, setNewExpense] = useState({ id: '', category: '', amount: '', date: new Date().toISOString().slice(0, 10) });
+    const [newExpense, setNewExpense] = useState({ category: '', amount: '', date: new Date().toISOString().slice(0, 10) });
+    const [editingExpenseId, setEditingExpenseId] = useState(null);
+    const [editExpense, setEditExpense] = useState({ category: '', amount: '', date: new Date().toISOString().slice(0, 10) });
+    const [showAddExpense, setShowAddExpense] = useState(false); 
 
-    const handleInputChange = (event, setter) => {
-        setter(prev => ({ ...prev, [event.target.name]: event.target.value }));
+    const handleInputChange = (event, field) => {
+        setNewExpense(prev => ({ ...prev, [field]: event.target.value }));
         refreshSession();
     };
 
-    const handleSelectChange = (event, setter) => {
-        setter(prev => ({ ...prev, [event.target.name]: event.target.value }));
+    const toggleAddExpense = () => {
+        setShowAddExpense(!showAddExpense);
+        setNewExpense({ category: '', amount: '', date: new Date().toISOString().slice(0, 10) }); 
         refreshSession();
     };
 
-    const handleEditExpense = (expense) => {
-        setNewExpense(expense);
-        refreshSession(); 
-    };
-
-    const handleAddOrUpdateExpense = () => {
-        const url = newExpense.id ? `${API_URL}/api/expenses/${user.id}/${newExpense.id}` : `${API_URL}/api/expenses`;
-        const method = newExpense.id ? 'put' : 'post';
+    const handleAddExpense = () => {
+        const url = `${API_URL}/api/expenses`;
         refreshSession();
-        axios[method](url, { userId: user.id, category: newExpense.category, amount: newExpense.amount, date: newExpense.date })
+        axios.post(url, { userId: user.id, ...newExpense })
             .then(() => {
-                setAlertMessage({ text: `${newExpense.id ? 'Updated' : 'Added'} expense successfully!`, type: 'success' });
-                setNewExpense({ id: '', category: '', amount: '', date: new Date().toISOString().slice(0, 10) });
+                setAlertMessage({ text: 'Added expense successfully!', type: 'success' });
+                fetchBudgetsAndExpenses();
+                setNewExpense({ category: '', amount: '', date: new Date().toISOString().slice(0, 10) }); 
+                toggleAddExpense();
+            })
+            .catch(error => {
+                setAlertMessage({ text: 'Failed to add expense.', type: 'danger' });
+            });
+    };
+
+    const startEdit = (expense) => {
+        setEditingExpenseId(expense.id);
+        setEditExpense({ ...expense });
+        refreshSession();
+    };
+
+    const handleInputChangeForEdit = (event, field) => {
+        setEditExpense(prev => ({ ...prev, [field]: event.target.value }));
+        refreshSession();
+    };
+
+    const handleSaveEdit = () => {
+        const url = `${API_URL}/api/expenses/${user.id}/${editExpense.id}`;
+        refreshSession();
+        axios.put(url, { userId: user.id, ...editExpense })
+            .then(() => {
+                setAlertMessage({ text: 'Updated expense successfully!', type: 'success' });
+                stopEdit();
                 fetchBudgetsAndExpenses();
             })
-            .catch(error => setAlertMessage({ text: `Failed to ${newExpense.id ? 'update' : 'add'} expense.`, type: 'danger' }));
+            .catch(error => {
+                setAlertMessage({ text: 'Failed to update expense.', type: 'danger' });
+            });
+    };
+
+    const stopEdit = () => {
+        setEditingExpenseId(null);
+        setEditExpense({ category: '', amount: '', date: new Date().toISOString().slice(0, 10) });
+        refreshSession();
     };
 
     const handleDeleteExpense = (id) => {
-        if (window.confirm('Are you sure you want to delete this expense?')) {
-            const newExpenses = expenses.filter(expense => expense.id !== id);
-            setExpenses(newExpenses);
-            refreshSession();
-
-            axios.delete(`${API_URL}/api/expenses/${user.id}/${id}`)
-                .then(() => {
-                    setAlertMessage({ text: 'Expense deleted successfully!', type: 'success' });
-                })
-                .catch(error => {
-                    setAlertMessage({ text: 'Failed to delete expense.', type: 'danger' });
-                    setExpenses(expenses);
-                });
-        }
+        refreshSession();
+        axios.delete(`${API_URL}/api/expenses/${user.id}/${id}`)
+            .then(() => {
+                setAlertMessage({ text: 'Expense deleted successfully!', type: 'success' });
+                fetchBudgetsAndExpenses();
+            })
+            .catch(error => {
+                setAlertMessage({ text: 'Failed to delete expense.', type: 'danger' });
+            });
     };
 
     return (
         <div>
             <h2>Expenses</h2>
-            <ul>
-                {expenses.map(expense => (
-                    <li key={expense.id}>
-                        {expense.category} - ${expense.amount} on {new Date(expense.date).toLocaleDateString()}
-                        <button onClick={() => handleEditExpense(expense)} className="btn btn-info">Edit</button>
-                        <button onClick={() => handleDeleteExpense(expense.id)} className="btn btn-danger">Delete</button>
-                    </li>
-                ))}
-            </ul>
-            <div>
-                <select name="category" value={newExpense.category} onChange={(e) => handleSelectChange(e, setNewExpense)}>
-                    <option value="">Select a Category</option>
-                    {budgets.map((budget) => (
-                        <option key={budget.id} value={budget.category}>{budget.category}</option>
+            <table className="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th className="col-3">Category</th>
+                        <th className="col-3">Amount</th>
+                        <th className="col-3">Date</th>
+                        <th className="col-3">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {expenses.map(expense => (
+                        <tr key={expense.id}>
+                            <td className="col-3">
+                                {editingExpenseId === expense.id ? (
+                                    <select 
+                                        value={editExpense.category} 
+                                        onChange={(e) => handleInputChangeForEdit(e, 'category')} 
+                                        className="form-control">
+                                        {budgets.map((budget) => (
+                                            <option key={budget.id} value={budget.category}>{budget.category}</option>
+                                        ))}
+                                    </select>
+                                ) : (
+                                    <span onClick={() => startEdit(expense)}>{expense.category}</span>
+                                )}
+                            </td>
+                            <td className="col-3">
+                                {editingExpenseId === expense.id ? (
+                                    <input 
+                                        type="number"
+                                        value={editExpense.amount} 
+                                        onChange={(e) => handleInputChangeForEdit(e, 'amount')} 
+                                        className="form-control"
+                                    />
+                                ) : (
+                                    <span onClick={() => startEdit(expense)}>${parseFloat(expense.amount)}</span>
+                                )}
+                            </td>
+                            <td className="col-3">
+                                {editingExpenseId === expense.id ? (
+                                    <input 
+                                        type="date"
+                                        value={editExpense.date}
+                                        onChange={(e) => handleInputChangeForEdit(e, 'date')} 
+                                        className="form-control"
+                                    />
+                                ) : (
+                                    <span onClick={() => startEdit(expense)}>{new Date(expense.date).toLocaleDateString()}</span>
+                                )}
+                            </td>
+                            <td className="col-3">
+                                {editingExpenseId === expense.id ? (
+                                    <>
+                                        <button onClick={handleSaveEdit} className="btn btn-success">Save</button>
+                                        <button onClick={stopEdit} className="btn btn-danger">Cancel</button>
+                                    </>
+                                ) : (
+                                    <button onClick={() => handleDeleteExpense(expense.id)} className="btn btn-danger">Delete</button>
+                                )}
+                            </td>
+                        </tr>
                     ))}
-                </select>
-                <input type="number" name="amount" placeholder="Amount" value={newExpense.amount} onChange={(e) => handleInputChange(e, setNewExpense)} />
-                <input type="date" name="date" value={newExpense.date} onChange={(e) => handleInputChange(e, setNewExpense)} />
-                <button onClick={handleAddOrUpdateExpense} className="btn btn-success">{newExpense.id ? 'Update' : 'Add'} Expense</button>
-            </div>
+                </tbody>
+            </table>
+            {showAddExpense ? (
+                <div className="mt-2 row">
+                    <div className="col">
+                        <select name="category" value={newExpense.category} onChange={(e) => handleInputChange(e, 'category')} className="form-control">
+                            <option value="">Category</option>
+                            {budgets.map(budget => (
+                                <option key={budget.id} value={budget.category}>{budget.category}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="col">
+                        <input type="number" name="amount" placeholder="Amount" value={newExpense.amount} onChange={(e) => handleInputChange(e, 'amount')} className="form-control" />
+                    </div>
+                    <div className="col">
+                        <input type="date" name="date" placeholder="Date" value={newExpense.date} onChange={(e) => handleInputChange(e, 'date')} className="form-control" />
+                    </div>
+                    <div className="col-auto">
+                        <button onClick={handleAddExpense} className="btn btn-success">Add Expense</button>
+                        <button onClick={toggleAddExpense} className="btn btn-danger">Cancel</button>
+                    </div>
+                </div>
+            ) : (
+                <button onClick={toggleAddExpense} className="btn btn-success">+ Add Expense</button>
+            )}
         </div>
     );
 }
